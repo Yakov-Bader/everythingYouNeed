dae0b10b46216921f71caa5b6f157d6fcdfc3499
Yakov <yakovbader@gmail.com>
2024-01-20 23:39:48 +0200

    backend dockerized

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -73,6 +73,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'PostgreSQL', 
+#         'USER': 'postgres', 
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost', 
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
